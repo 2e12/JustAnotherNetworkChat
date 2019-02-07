9f4ceae23e77f75bd93ddad20c52d4c1f0513e7b
bbeutg <gabriel.beutler@bbcag.ch>
2019-02-07 12:25:50 +0100

    test Commit

--- src/sample/JancProtocolHandler.java
+++ src/sample/JancProtocolHandler.java
@@ -1,4 +1,37 @@
 package sample;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class JancProtocolHandler {
+    //Map for the protocl commands
+    static Map<String, Class> commands = new HashMap<>();
+
+    public JancProtocolHandler(){
+        JancProtocolHandler.registerCommand("lgn", RequestSessionKey.class.getClass());
+    }
+
+    //Parse the Command and passing the Arguments to the registerd command
+    public void ParseFromString(String input){
+        String[] parts = input.split(";");
+    }
+
+    private void ExecuteCommand(String[] inputParts){
+        try {
+            JancCommand obj = (JancCommand) commands.get("req").getConstructors()[0].newInstance("");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void registerCommand(String commandName, Class command){
+        if (command.getSuperclass() == JancCommand.class) {
+            commands.put(commandName, command);
+        }
+    }
 }
--- src/sample/RequestSessionKey.java
+++ src/sample/RequestSessionKey.java
@@ -1,4 +1,21 @@
 package sample;
 
-public class RequestSessionKey {
+import java.net.Socket;
+
+public class RequestSessionKey extends JancCommand{
+
+    @Override
+    void handle() {
+
+    }
+
+    @Override
+    void send(Socket socket) {
+
+    }
+
+    @Override
+    void parseFromString(String input) {
+
+    }
 }
