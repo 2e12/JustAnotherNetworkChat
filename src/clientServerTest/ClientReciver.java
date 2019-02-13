049a3d9c7c9fec69e3e524c9dc949446ad5aa544
bbeutg <gabriel.beutler@bbcag.ch>
2019-02-13 07:27:21 +0100

    Added "Listener" functionalities to Test

--- src/clientServerTest/ClientReciver.java
+++ src/clientServerTest/ClientReciver.java
@@ -1,5 +1,30 @@
 package clientServerTest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class ClientReciver extends Thread{
-    public void run(){}
+
+    private Socket socket;
+
+
+    public ClientReciver(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+            while (!this.socket.isClosed()) {
+                String response = in.readLine();
+                if (response != null) {
+                    System.out.println(response);
+                }
+            }
+        } catch (IOException e) {
+
+        }
+    }
 }
--- src/clientServerTest/ClientTest.java
+++ src/clientServerTest/ClientTest.java
@@ -20,11 +20,14 @@ public class ClientTest {
             out.println("lgn;ADOLF;sml123456");
             //out.flush();
             Scanner scanner = new Scanner(System.in);
+            ClientReciver listener = new ClientReciver(socket);
+            listener.start(); //Runs in its own thread ;)
 
-            //while (!socket.isClosed()) {
-            //    String input = scanner.nextLine();
-            //    out.println("msg;null;gibb;null;" + System.currentTimeMillis() + ";" + input + ";");
-            //}
+            while (!socket.isClosed()) {
+                String input = scanner.nextLine();
+                out.println("msg;null;gibb;null;" + System.currentTimeMillis() + ";" + input + ";");
+            }
+
 
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             while (!socket.isClosed()) {
