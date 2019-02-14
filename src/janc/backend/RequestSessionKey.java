850bb282b2193c448c59b7ced01d651d4607fd69
bbeutg <gabriel.beutler@bbcag.ch>
2019-02-14 08:05:46 +0100

    Added Exception for Malformed Commands

--- src/janc/backend/RequestSessionKey.java
+++ src/janc/backend/RequestSessionKey.java
@@ -10,35 +10,38 @@ public class RequestSessionKey extends JancCommand{
     String username;
     String password;
 
-    public RequestSessionKey(String[] parts, Socket source) {
+    public RequestSessionKey(String[] parts, Socket source) throws MalformedCommandException {
         super(parts, source);
     }
 
     @Override
     void handle() {
         String[] parts = new String[]{"key", this.username, null};
-        SendSessionKey key = new SendSessionKey(parts, this.getSourceSocket());
         try {
-            User user = new User();
-            user.setUsername(this.username);
-            user.setPassword(this.password);
-            UserJDBCDao userLogin = new UserJDBCDao(ConnectionFactory.getInstance().getConnection());
+            SendSessionKey key = new SendSessionKey(parts, this.getSourceSocket());
+            try {
+                User user = new User();
+                user.setUsername(this.username);
+                user.setPassword(this.password);
+                UserJDBCDao userLogin = new UserJDBCDao(ConnectionFactory.getInstance().getConnection());
 
-            loginstate state = userLogin.checkLoginCredentials(user);
-            if (state != loginstate.worngpassword) {
-                if (state == loginstate.nouser) {
-                    userLogin.insertUser(user);
+                loginstate state = userLogin.checkLoginCredentials(user);
+                if (state != loginstate.worngpassword) {
+                    if (state == loginstate.nouser) {
+                        userLogin.insertUser(user);
+                    }
+                    key.send(this.getSourceSocket());
+                    JancProtocolHandler.getInstance().putUserConnection(this.username, this.getSourceSocket(), null);
+                    System.out.println("[Login] Hello " + this.username + this.getSourceSocket().getInetAddress());
+                } else {
+                    getSourceSocket().close();
                 }
-                key.send(this.getSourceSocket());
-                JancProtocolHandler.getInstance().putUserConnection(this.username, this.getSourceSocket(), null);
-                System.out.println("[Login] Hello " + this.username + this.getSourceSocket().getInetAddress());
-            } else {
-                getSourceSocket().close();
+            } catch (IOException | SQLException e) {
+
             }
-        } catch (IOException | SQLException e) {
+        } catch (MalformedCommandException me) {
 
         }
-
     }
 
     @Override
@@ -50,10 +53,12 @@ public class RequestSessionKey extends JancCommand{
 
     @Override
         //Set Parameters
-    void parseFromString(String[] parts) {
+    void parseFromString(String[] parts) throws MalformedCommandException {
         if(parts.length == 3) {
             this.username = parts[1];
             this.password = parts[2];
+        } else {
+            throw new MalformedCommandException("Wrong number of Paramteres");
         }
     }
 }
