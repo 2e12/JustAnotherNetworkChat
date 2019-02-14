ff85f57f7b530d765bee0a1127986b9ed48990a2
bzehni <ivan.zehnder@bbcag.ch>
2019-02-14 10:33:51 +0100

    BEST COMMIT IN MY LIFE

--- src/janc/frontend/View.java
+++ src/janc/frontend/View.java
@@ -14,7 +14,7 @@ public class View{
         this.controller = controller;
         this.model = model;
         home = new Home();
-        chat = new Chat();
+        chat = new Chat(controller);
         registerButtonActionHandler();
         listenToModelChanges();
     }
@@ -53,6 +53,15 @@ public class View{
     }
 
     private void listenToModelChanges() {
+        model.srollProperty().addListener((observable, oldValue, newValue) ->
+        {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            chat.getSpPane().setVvalue(1.0);
+        });
         model.changeSiteProperty().addListener((observable, oldValue, newValue) ->
                 Client.switchToScene());
         model.writtenMessageProperty().addListener((observable, oldValue, newValue) ->
